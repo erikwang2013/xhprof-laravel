6e5200ea93a63731178ac32327a99cd4d8d1bcef
erik@erik.xyz <erikwang2013>
2023-10-29 21:47:38 +0800

    更新配置

--- src/Xhprof/Middleware/XhprofMiddleware.php
+++ src/Xhprof/Middleware/XhprofMiddleware.php
@@ -15,16 +15,15 @@ class XhprofMiddleware {
 
     public function handle(Request $request, callable $next): Response
     {
-        $config=config('xhprof');
-        $xhprof =$config['enable']?:false;
+        $xhprof =config('xhprof.enable')?:false;
         $extension = extension_loaded('xhprof');
         if(false==$extension) return response()->withBody("请安装xhprof扩展");
         $redis=extension_loaded("redis");
         if(false==$redis) return response()->withBody("请安装redis扩展");
-        Xhprof::$ignore_url_arr=$config['ignore_url_arr']?:"/test";
-        Xhprof::$time_limit=$config['time_limit']?:0;
-        Xhprof::$log_num=$config['log_num']?:1000;
-        Xhprof::$view_wtred=$config['view_wtred']?:3;
+        Xhprof::$ignore_url_arr=config('xhprof.ignore_url_arr')?:"/test";
+        Xhprof::$time_limit=config('xhprof.time_limit')?:0;
+        Xhprof::$log_num=config('xhprof.log_num')?:1000;
+        Xhprof::$view_wtred=config('xhprof.view_wtred')?:3;
         if ($xhprof && $extension) Xhprof::xhprofStart();
         $response = $next($request);
         if ($xhprof && $extension) Xhprof::xhprofStop();
